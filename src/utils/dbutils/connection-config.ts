c479341289994415cc9fd24376b135536bb93151
Shakeel Osmani <shakeel.osmani@gmail.com>
2020-12-04 14:28:20 -0500

    change

--- src/utils/dbutils/connection-config.ts
+++ src/utils/dbutils/connection-config.ts
@@ -2,7 +2,7 @@ import { config } from 'mssql';
 
 const ConnectionConfig: config = {
   user: process.env.DBUSER || 'sqlserveruser',
-  password: process.env.DBPASSWORD || 'Infotech786',
+  password: process.env.DBPASSWORD || ' ',
   server: process.env.DBSERVER || '192.168.68.112',
   port: Number(process.env.DBPORT) || 1433,
   database: process.env.DB || 'test'
